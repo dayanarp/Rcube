99bfd0a75ce4636592922ee11a7ab2a2129c0a34
Dayana Rodrigues <dayana2301@gmail.com>
2016-11-21 10:46:47 -0400

    arreglados bugs de xml

---
+++ app/src/main/java/com/example/dayana/myapplication/MainActivityU.java
@@ -0,0 +1,13 @@
+package com.example.dayana.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_u);
+    }
+}
